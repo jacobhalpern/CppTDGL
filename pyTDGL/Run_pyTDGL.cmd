09700eafed6d5e0c685f1949cde9ee867791f128
jacobhalpern <77133248+jacobhalpern@users.noreply.github.com>
2026-08-09 12:01:05 -0400

    Removed pyTDGL to local reference folder

--- pyTDGL/Run_pyTDGL.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set "PROJECT_ROOT=%~dp0"
-set "PYTDGL_PY=%USERPROFILE%\.venvs\pyTDGL\Scripts\python.exe"
-
-cd /d "%PROJECT_ROOT%"
-
-"%PYTDGL_PY%" -m notebook "%PROJECT_ROOT%docs\notebooks\quickstart.ipynb"
-pause
